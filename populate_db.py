fdf36043fb042b552be8b9d3889ca10bc91b1846
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-10-06 20:01:28 +0530

    FIX: Index

--- populate_db.py
+++ populate_db.py
@@ -5,9 +5,4 @@ from sqlalchemy import create_engine
 df = pd.read_csv("input.csv")
 df["verified"] = 0
 db = create_engine("sqlite:///OSM_Tamil_translation_TN.db", echo=False)
-df.to_sql(
-    "translation",
-    db,
-    if_exists="append",
-    index=False,
-    index_label='OSM_ID')
+df.to_sql("translation", db, if_exists="append")
