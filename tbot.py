480a11f992fa53fc7f40cfc662217230638d5e02
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-10-06 18:47:08 +0530

    ENH: Connecting DB

--- tbot.py
+++ tbot.py
@@ -1,10 +1,24 @@
 """TelegramBot for OSM translation."""
 import telebot
-import sqlite3
-import pandas as pd
 from telebot import types
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import *
 
 bot = telebot.TeleBot("TOKEN")
+db = create_engine("sqlite:///OSM_Tamil_translation_TN.db", echo=False)
+Base = declarative_base()
+Base.metadata.reflect(db)
+Session = sessionmaker(bind=db)
+session = Session()
+
+
+class data(Base):
+    """Class for translation table."""
+    __table__ = Base.metadata.tables['translation']
+    __mapper_args__ = {
+        'primary_key': [__table__.c.osm_id]
+    }
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -19,13 +33,20 @@ You can control me by sending these commands:
 /verify - To verify translations.""")
 
 
-@bot.message_handler(commands=['translate'])
-def get_translate(message):
-    """/translate."""
+@bot.message_handler(commands=['verify'])
+def get_verified(message):
+    """/verify."""
+    bot.send_chat_action(message.chat.id, 'typing')
+    result = session.query(data).filter_by(verified=0).first()
+    text = "Is %s a correct translation of %s" % (result.translation, result.name)
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add('True', 'False')
+    markup.add('Correct', 'Wrong')
     msg = bot.send_message(
-        message.chat.id, 'Lets start translating', reply_markup=markup)
+        message.chat.id, text, reply_markup=markup)
+    bot.register_next_step_handler(msg, commit_verify)
 
+
+def commit_verify(message):
+    print(message)
 
 bot.polling()
