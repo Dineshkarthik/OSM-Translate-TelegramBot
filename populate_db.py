a96b332c810f6364a73fe6ec11ea7de2da79dcd9
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-10-07 07:01:32 +0530

    ENH: Adding Config

--- populate_db.py
+++ populate_db.py
@@ -1,8 +1,16 @@
 """Script to populate db."""
+import os
+import yaml
 import pandas as pd
 from sqlalchemy import create_engine
 
-df = pd.read_csv("input.csv")
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+f = open(os.path.join(THIS_DIR, 'config.yaml'))
+config = yaml.safe_load(f)
+f.close()
+
+
+df = pd.read_csv(config["input_csv"])
 df["verified"] = 0
-db = create_engine("sqlite:///OSM_Tamil_translation_TN.db", echo=False)
+db = create_engine("sqlite:///" + config["db_name"] + ".db", echo=False)
 df.to_sql("translation", db, if_exists="append")
